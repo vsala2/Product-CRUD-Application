4ab2b607707429a7f2272893c7d78511966781a6
Vaishnavi Salaskar <vaishnavisalaskar0@gmail.com>
2024-01-24 00:09:41 -0500

    add product module + front end part1

--- productcrudapp/src/main/java/productcrudapp/controller/MainController.java
+++ productcrudapp/src/main/java/productcrudapp/controller/MainController.java
@@ -1,13 +1,48 @@
 package productcrudapp.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import productcrudapp.Dao.ProductDao;
+import productcrudapp.model.Product;
 
 @Controller
 public class MainController {
 	
+	//to save the data in the DB we need Dao object.
+	@Autowired
+	private ProductDao productDao;
+	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model m) {
+		List<Product> products = productDao.getProducts();
+		m.addAttribute("product", products);
 		return "index";
+	}
+	
+	//to add product
+	@RequestMapping("/add-product")
+	public String addProduct(Model m) {
+		m.addAttribute("title", "Add Product!");
+		return "add_product_form";
+	}
+	
+	//handle- add product form
+	@RequestMapping(value="/handle-product", method= RequestMethod.POST)
+	public RedirectView handleProduct(@ModelAttribute Product product, HttpServletRequest request) {
+		System.out.println(product);
+		productDao.createProduct(product);
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(request.getContextPath() + "/");
+		return redirectView;
 	}
 }
--- productcrudapp/src/main/webapp/WEB-INF/views/index.jsp
+++ productcrudapp/src/main/webapp/WEB-INF/views/index.jsp
@@ -2,12 +2,49 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Home</title>
-</head>
-<body>
-	<h1>This is My first page.</h1>
+	<head>
+	<meta charset="UTF-8">
+	<!-- jstl -->
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	<%@ include file="./base.jsp" %>
+	<title>Home</title>
+	</head>
 
-</body>
+	<body>
+		
+		<div class="container mt-3">
+	
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="text-center mb-3">Welcome to Product App!</h1>
+					
+					<table class="table table-striped table-dark">
+					  <thead>
+					    <tr>
+					      <th scope="col">Sr. No</th>
+					      <th scope="col">Product Name</th>
+					      <th scope="col">Description</th>
+					      <th scope="col">Price</th>
+					      <th scope="col">Action</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+					      <th scope="row">1</th>
+					      <td>Mark</td>
+					      <td>Otto</td>
+					      <td>@mdo</td>
+					    </tr>
+
+					  </tbody>
+					</table>
+					
+					<div class="container text-center">
+						<a href="add-product" class="btn btn-primary">Add Product</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+	</body>
 </html>
